6e5fa759f533de8b1605170485c5a22d49ef4948
mehdizoghinia <mehdizowghi@gmail.com>
2023-10-27 11:39:49 -0400

    feat: setup api, mailhog postgres containers

---
+++ backendApi/settings/local.py
@@ -0,0 +1,14 @@
+
+from .base import *  # noqa
+from .base import env
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="XQM9OxpwX-vNMbKVHykihvLAb8xkkmS2jilqBGujQ1VgPfhGNQk",
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
