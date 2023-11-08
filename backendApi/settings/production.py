6960b63a0488e41389a07cae219febb25ebdac25
mehdizoghinia <mehdizowghi@gmail.com>
2023-11-08 18:57:04 -0500

    feat: add profile, follow , unfollow

--- backendApi/settings/production.py
+++ backendApi/settings/production.py
@@ -5,3 +5,8 @@ ADMINS = [("Mehdi Zoghinia", "mehdizowghi@gmail.com")]
 
 # TODO add domain names of the production server
 CSRF_TRUSTED_ORIGINS = []
+
+DEFAULT_FROM_EMAIL = env(
+    "DJANGO_DEFAULT_FROM_EMAIL",
+    default="Authors Haven Support <support@trainingwebdev.com>",
+)
